8296c2f651ea08f305d26785d318feb5577ea2b4
abiramisenthil <abis.senthil@gmail.com>
2019-11-14 11:22:25 +0000

    Add files via upload

---
+++ AdapterPattern/AdapterDesignPattern/AdapterDesignPattern/Adapter/EmployeeAdapter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using AdapterDesignPattern.Target;
+using Newtonsoft.Json;
+
+namespace AdapterDesignPattern.Adapter
+{
+    public class EmployeeAdapter : ITarget
+    {
+        private EmployeeDetails _employeeDetails;
+
+        public EmployeeAdapter(EmployeeDetails empDetails)
+        {
+            _employeeDetails = empDetails;
+        }
+
+        public List<string> GetEmployeeList()
+        {
+            List<string> employeeList = new List<string>();
+            string[][] employees = _employeeDetails.GetEmployees();
+            foreach (string[] employee in employees)
+            {
+                employeeList.Add(employee[0]);
+                employeeList.Add(",");
+                employeeList.Add(employee[1]);
+                employeeList.Add(",");
+                employeeList.Add(employee[2]);
+                employeeList.Add("\n");
+            }
+
+            return employeeList;
+        }
+    }
+}
---
+++ AdapterPattern/AdapterDesignPattern/AdapterDesignPattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using AdapterDesignPattern.Adapter;
+using AdapterDesignPattern.Target;
+
+namespace AdapterDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /* Client needs Employee details in list format. The existing class(employeeDetails) method GetEmployees returns employeedetails in array of string
+            So the existing class is incompatible to satisfy client request.
+
+            */
+            ITarget emp = new EmployeeAdapter(new EmployeeDetails());
+            List<string> emplist = emp.GetEmployeeList();
+            foreach (var item in emplist)
+            {
+                Console.Write(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
